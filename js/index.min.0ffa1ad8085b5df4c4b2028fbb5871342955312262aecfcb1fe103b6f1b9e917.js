5a3b56d044f4e589de0237af32e457d691a3907f
Mohseen <mohseenmukaddam6@gmail.com>
2021-12-31 18:26:37 -0800

    [update] adding new content

--- js/index.min.0ffa1ad8085b5df4c4b2028fbb5871342955312262aecfcb1fe103b6f1b9e917.js
+++
@@ -1,4 +0,0 @@
-'use strict';document.addEventListener('DOMContentLoaded',function(){function toggleMobileNavState(){var body=document.querySelector("body");body.classList.toggle("nav--active");}
-function initBurger(){var burger=document.querySelector(".burger");burger.addEventListener("click",toggleMobileNavState);}
-var toggle=document.getElementById('theme-toggle');toggle.addEventListener('change',function(e){if(e.target.checked){document.documentElement.setAttribute('data-theme','dark');return;}
-document.documentElement.setAttribute('data-theme','light');});initBurger();});
